6839912b8f91bf4c513e9c78af7aceff0f52c585
default <dstepbw@gmail.com>
2016-05-18 01:14:26 -0300

    fix inject

--- src/angular-desktop-notification.js
+++ src/angular-desktop-notification.js
@@ -4,7 +4,7 @@
   .module('DesktopNotification', [])
   .factory('notification', notification);
   
-   notification.$injec = [];
+   notification.$inject = [];
    function notification(){
     var factory = {
       push : push,
